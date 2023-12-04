6ea543f2ca8608317d5cf6b1e32d7bdc74a79ea0
Ali <ibrahimzade.ali99@gmail.com>
2023-12-04 13:34:00 +0400

    image repo, util, service and model are uploaded

---
+++ src/main/java/az/office/officenter/exception/ContextRuntimeException.java
@@ -0,0 +1,7 @@
+package az.office.officenter.exception;
+
+public class ContextRuntimeException extends RuntimeException{
+    public ContextRuntimeException(String message) {
+        super(message);
+    }
+}
